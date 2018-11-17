f7db25472cf3ceacef62a8beb7284b89294323c0
theangi <matteo.angiari@gmail.com>
2018-11-17 10:00:48 +0100

    hourglass sum (parametric) + minimaxsum

--- algorithms/warmup/05_MiniMaxSum.py
+++ algorithms/warmup/05_MiniMaxSum.py
@@ -1,11 +1,9 @@
 #!/bin/python3
 
-# Complete the miniMaxSum function below.
-def miniMaxSum(arr):
+def mini_max_sum(arr):
     print(sum(sorted(arr)[:-1]), sum(sorted(arr, reverse=True)[:-1]))
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
-    miniMaxSum(arr)
-    
+    mini_max_sum(arr)
